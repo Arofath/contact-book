b258dce555b6a90af54d3154aa51d6ade75bdcff
Arofath <suty2689@gmail.com>
2026-05-25 10:30:53 +0700

    remove conditon

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ from src.controllers.contact_controller import contact_router
 load_dotenv()
 
 app_env = os.getenv('APP_ENV')
-is_local = (app_env == 'production')
+is_local = (app_env == 'local')
 
 app = FastAPI(
     title='Contact Book API',
     version='1.0.0',
     description='All API endpoints for Contact Book',
-    docs_url="/docs" if is_local else None, 
-    redoc_url="/redoc" if is_local else None
+    docs_url="/docs",  # បើកជានិច្ច
+    redoc_url="/redoc" # បើកជានិច្ច
 )
 app.mount('/upload', StaticFiles(directory='upload'), name='upload')
 
